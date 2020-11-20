5bcf98053a7a580b7c1008789c7e75e8119bb1fd
ibalnst <50813760+ibalnst@users.noreply.github.com>
2020-11-21 00:34:20 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ $('#year').text(new Date().getFullYear());
+
+ $('.port-item').click(() => {
+     $('.collapse').collapse('hide');
+ });
+
+ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+     event.preventDefault();
+     $(this).ekkoLightbox();
+ });
